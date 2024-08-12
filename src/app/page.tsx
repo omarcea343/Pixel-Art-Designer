a485efd6cfea7927bee71bc2f1ed21585a76d01e
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-08-12 13:56:26 -0600

    feat: Remove unused SVG files and add Background component to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,22 @@ import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Background />
-      <div className="flex flex-col items-center gap-2 justify-center">
-        <h1 className="font-bold text-4xl text-primary">Pixel Art Designer</h1>
-        <p className="flex text-muted-foreground text-sm">
-          You can change the grid size, color size and make something amazing.
-        </p>
+    <section className="flex flex-col items-center justify-center">
+      <div className="flex flex-col mt-14">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h1 className="font-bold text-4xl text-primary">
+            Pixel Art Designer
+          </h1>
+          <p className="flex text-muted-foreground text-sm">
+            You can change the grid size, color size and make something amazing.
+          </p>
+        </div>
+        <div className="flex mt-8">
+          <Card className="flex w-[40rem] h-[41rem] items-center justify-center">
+            <PixelArtMaker />
+          </Card>
+        </div>
       </div>
-      <Card className="flex w-[40rem] h-[41rem] items-center justify-center">
-        <PixelArtMaker />
-      </Card>
-    </main>
+    </section>
   );
 }
