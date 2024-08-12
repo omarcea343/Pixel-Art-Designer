6646f6cc88914fb30f41b8c033fb43a3fd8242f5
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-08-12 10:00:24 -0600

    Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Background from "@/components/Background";
 import PixelArtMaker from "@/components/PixelArtMaker";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PixelArtMaker />
+      <Background />
+      <div className="flex flex-col items-center gap-2 justify-center">
+        <h1 className="font-bold text-4xl text-primary">Pixel Art Designer</h1>
+        <p className="flex text-muted-foreground text-sm">
+          You can change the grid size, color size and make something amazing.
+        </p>
+      </div>
+      <Card className="flex w-[40rem] h-[40rem] items-center justify-center">
+        <PixelArtMaker />
+      </Card>
     </main>
   );
 }
--- src/components/PixelArtMaker.tsx
+++ src/components/PixelArtMaker.tsx
@@ -1,3 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+
+const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_GRID_COLOR = "#FFF";
+const DEFAULT_SELECTED_COLOR = "#000";
+
 export default function PixelArtMaker() {
-  return <div>PixelArtMaker</div>;
+  const [gridSize, setGridSize] = useState<number>(DEFAULT_GRID_SIZE);
+  const [grid, setGrid] = useState<string[]>(
+    Array(gridSize * gridSize).fill(DEFAULT_GRID_COLOR)
+  );
+
+  const [selectedColor, setSelectedColor] = useState<string>(
+    DEFAULT_SELECTED_COLOR
+  );
+
+  const handleCellClick = (index: number) => {
+    const newGrid = [...grid];
+    newGrid[index] = selectedColor;
+    setGrid(newGrid);
+  };
+
+  const handleGridChange = (newGridSize: number) => {
+    setGridSize(newGridSize);
+    setGrid(Array(newGridSize * newGridSize).fill(DEFAULT_GRID_COLOR));
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <div className="flex gap-3">
+        <Label>Pick Grid Size:</Label>
+        <Input
+          type="number"
+          value={gridSize}
+          onChange={(e) => handleGridChange(+e.target.value)}
+          className="text-black"
+        />
+        <Label>Pick Color:</Label>
+        <Input
+          type="color"
+          value={selectedColor}
+          onChange={(e) => setSelectedColor(e.target.value)}
+        />
+      </div>
+      <div
+        className="grid w-[30rem] h-[30rem]"
+        style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
+      >
+        {grid.map((color, index) => (
+          <div
+            key={index}
+            onClick={() => handleCellClick(index)}
+            className="border"
+            style={{ backgroundColor: color }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
